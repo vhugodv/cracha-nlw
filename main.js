dce7ae8149dc2074d1f45f2788cebd6571a78ddc
Dev Hugo <92953811+vhugodv@users.noreply.github.com>
2021-10-23 12:03:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'vhugodv',
+  youtube: 'UCheaZBQ55kI4ICEobwuRUvw',
+  facebook: 'victorhugo.santossilva.564',
+  instagram: 'megagames2020',
+  twitter: 'kougiss'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com.br/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
